a06e521355d57fcc21da2dfe4cbb5950710b4262
46nuko <152859182+46nuko@users.noreply.github.com>
2026-07-30 08:40:40 +0000

    testデータ挿入データ

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+olivia = User.find_or_create_by!(email_address: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+end
+
+james = User.find_or_create_by!(email_address: "james@example.com") do |user|
+  user.name = "James"
+  user.password = "password"
+end
+
+lucas = User.find_or_create_by!(email_address: "lucas@example.com") do |user|
+  user.name = "Lucas"
+  user.password = "password"
+end
+
+Post.find_or_create_by!(body: "今日は推しぬいとこの場所に遊びに来た！") do |post|
+  post.user = olivia
+end
+
+Post.find_or_create_by!(body: "今日も推しが可愛い！") do |post|
+  post.user = james
+end
+
+Post.find_or_create_by!(body: "推しとの写真を撮るのにおすすめ！") do |post|
+  post.user = lucas
+end
+
 Admin.create!(
   email_address: "admin@example.com",
   password: "password"
